94666389f698b8a84bf782416e89eca1d63bc33b
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-09-20 23:03:55 +0530

    Type error

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,17 +2,17 @@ import { useParams } from "react-router-dom";
 import { useBlog } from "../hook"
 import { FullBlog } from "../components/FullBlog";
 import { Appbar } from "../components/Appbar";
-import { Spinner } from "../components/spinner";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 
 export const Blog =() => {
     const { id } =useParams();
     const {loading,blog} =useBlog({
         id:id || ""
     });
-    if(loading){
+    if(loading || !blog){
         return <div> 
             <Appbar></Appbar>
-            <Spinner/>
+            <BlogSkeleton/>
         </div>
     }
     return (
